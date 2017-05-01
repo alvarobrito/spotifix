e6e25cf56acaacc63396c10c473074b2b7e13eb3
Alvaro Brito <alvarobr85@gmail.com>
2017-05-01 12:20:23 +0200

    fix(sidebar) sidebar into the router

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import SideBarContainer from '@/containers/SideBarContainer';
 
 const HomePage = () => (
   <div className="home-page">
     <h1>Home Page</h1>
-    <SideBarContainer />
   </div>
 );
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ import { ConnectedRouter } from 'react-router-redux';
 
 // Containers
 import SearchContainer from '@/containers/SearchContainer';
+import SideBarContainer from '@/containers/SideBarContainer';
 
 // Pages
 import HomePage from '@/pages/HomePage';
@@ -13,6 +14,7 @@ import LoginPage from '@/pages/LoginPage';
 const Routes = ({ history }) => (
   <ConnectedRouter history={history}>
     <div>
+      <SideBarContainer />
       <Route exact path="/" component={HomePage} />
       <Route path="/browse" component={SearchContainer} />
       <Route path="/login" component={LoginPage} />
