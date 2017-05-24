b7f8e102a684b72fa1004e7141c23a0b9b91d8e2
Ivan Rodriguez <frivanrodriguez@gmail.com>
2017-05-24 12:51:47 +0200

    #3 feat(search): Add a selector creator to use a deep equality check

--- src/modules/selectors/search.js
+++ src/modules/selectors/search.js
@@ -1,23 +1,42 @@
-import { createSelector } from 'reselect';
+import { createSelectorCreator, defaultMemoize } from 'reselect';
+import { isEqual } from 'lodash';
 
+// Create a "selector creator" that uses lodash.isEqual instead of ===
+const createDeepEqualSelector = createSelectorCreator(
+  defaultMemoize,
+  isEqual,
+);
+
+// Reducer to obtain an array with the results filtered and mapped
+const mapFiltered = (filterCondition, mapHandler) => (prev, next) => {
+  if (!filterCondition(next)) return prev;
+
+  return [...prev, mapHandler(next)];
+};
+
+// Entities
 const getTracks = state => state.entities.tracks;
 const getArtists = state => state.entities.artists;
 const getAlbums = state => state.entities.albums;
 
+// Sections
 const getSearchTracksIds = state => state.sections.search.tracks;
 
-export const getSearchTracks = createSelector(
+// Selectors
+export const getSearchTracks = createDeepEqualSelector(
   [getArtists, getAlbums, getTracks, getSearchTracksIds],
   (artists, albums, tracks, searchTracksIds) => searchTracksIds
-  .filter(trackId => tracks[trackId]
-    && albums[tracks[trackId].album])
-  .map(trackId => {
+  .reduce(mapFiltered(
+    trackId => tracks[trackId] && albums[tracks[trackId].album],
+    trackId => {
     return {
       ...tracks[trackId],
       album: albums[tracks[trackId].album],
       artists: tracks[trackId].artists
-        .filter(artistId => artists[artistId])
-        .map(artistId => artists[artistId]),
+        .reduce(mapFiltered(
+          artistId => artists[artistId],
+          artistId => artists[artistId]), []
+        ),
     };
-  }),
+  }), []),
 );
